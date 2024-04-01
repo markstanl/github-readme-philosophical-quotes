aa811d7f0c20d86b20293aec43b73555ee04becc
markstanl <markgstanley1@gmail.com>
2024-04-01 16:00:56 -0500

    Some cleanup on the comments and formatting of the file

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-# app.py
-
 from flask import Flask, send_file, request
 from PIL import Image, ImageDraw, ImageFont
 from textwrap import wrap
 import random
 import os
-from quotes import quotes_original  # Assuming quotes is a list of quotes
+from quotes import quotes_original
 from themes import themes
 
 app = Flask(__name__)
@@ -17,7 +15,8 @@ def generate_image():
     Generates an image with a random quote and author
 
     Args:
-        inputted_author (str): an author used to filter the quotes
+        author (str): an author used to filter the quotes
+        theme (str): a theme used to select the background image
 
     Returns:
         image: a generated image with a quote and author
@@ -26,10 +25,12 @@ def generate_image():
         404: If no quotes are found for the given author
     """
 
-    quotes = quotes_original  # Apparently necessary
+    quotes = quotes_original  # makes a mutable copy of the original quote list
 
     inputted_author = request.args.get('author', default=None, type=str)
     inputted_theme = request.args.get('theme', default=None, type=str)
+
+    inputted_theme = inputted_theme.lower() if inputted_theme is not None else None  # case insensitive
 
     if inputted_author is not None:  # Filter quotes by author
         filtered_quotes = [quote for quote in quotes if quote[1].lower() == inputted_author.lower()]
@@ -37,13 +38,11 @@ def generate_image():
             return "No quotes found for this given author", 404
         else:
             quotes = filtered_quotes
-    inputted_theme = inputted_theme.lower() if inputted_theme is not None else None
 
     theme = themes.get(inputted_theme, themes["default"])
     image_path = theme[0]
 
-    quote, author = random.choice(quotes)
-    author = f'- {author}'
+    quote, author = random.choice(quotes)  # get the random quote and author from the quoate list
 
     # Load background image
     background_image = Image.open(image_path)
@@ -71,6 +70,7 @@ def generate_image():
     quote_font = ImageFont.truetype(quote_font_path, size=quote_font_size)
 
     size = 30
+    author = f'- {author}'  # Add a dash before the author's name as it will be displayed on the imageq
     if len(author) < 8:  # adjust the x distance based on the length of the author's name
         x_distance = 220
     elif len(author) < 12:
@@ -84,8 +84,8 @@ def generate_image():
     else:
         x_distance = 80
         size = 25
-
     author_font = ImageFont.truetype(author_font_path, size=size)
+
     # Calculate the height of the text box
     text_height = sum(
         draw.textbbox((0, 0), line, font=quote_font)[3] - draw.textbbox((0, 0), line, font=quote_font)[1] for line in
@@ -95,26 +95,18 @@ def generate_image():
     move_up_by = 50
     quote_y = ((background_image.height - text_height) // 2) - move_up_by
 
-    # Set text color
-    text_color = theme[1]
+    text_color = theme[1]  # author text color
     draw.text((x_distance, 175), author, fill=text_color, font=author_font)  # Draw the author's name on the image
-    text_color = theme[2]
+    text_color = theme[2]  # quote text color
 
-    for line in wrapped_quote:
-        # Calculate x position for this line
+    for line in wrapped_quote:  # Draw the quote on the image
         x = (background_image.width - draw.textbbox((0, 0), line, font=quote_font)[2]) // 2
-
-        # Draw the text on the image
         draw.text((x, quote_y), line, fill=text_color, font=quote_font)
-
-        # Update the y position for the next line
         quote_y += draw.textbbox((0, 0), line, font=quote_font)[3] - draw.textbbox((0, 0), line, font=quote_font)[1]
 
-    # Save the modified image
+    # Save and return the modified image
     output_image_path = os.path.join(os.getcwd(), "output_image.jpg")
     background_image.save(output_image_path)
-
-    # Send the image file to the client
     return send_file(output_image_path, mimetype='image/jpeg')
 
 
